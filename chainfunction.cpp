924e116ba2bb04ffd9f87bab1c91cdb5049781cc
NurulNajatMony <nurulmony505@gmail.com>
2025-05-17 15:56:03 +0700

    menambahkan pemanggilan methode di fungsi main

--- chainfunction.cpp
+++ chainfunction.cpp
@@ -17,4 +17,12 @@ public:
 
 } bukunya;
 
+int main()
+{
+    // bukunya.setJudul("Matematika");
+    // cout << bukunya.getJudul();
+    cout << bukunya.setJudul("Matematika").getJudul(); // chain function calls
+    return 0;
+}
+
 
