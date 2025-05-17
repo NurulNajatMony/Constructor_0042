41615a2245069f89f79a550c1518cfdfe677dd52
NurulNajatMony <nurulmony505@gmail.com>
2025-05-17 16:03:34 +0700

    mendeklarasi class angka

--- destruktor.cpp
+++ destruktor.cpp
@@ -1,3 +1,13 @@
 #include <iostream>
 using namespace std;
 
+class angka{
+private:
+    int *arr;
+    int panjang;
+public:
+    angka(int); //Contructor
+    ~angka(); //Destructor
+    void cetakData();
+    void isiData();
+};
