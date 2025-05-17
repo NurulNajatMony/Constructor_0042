b8260f59690b9f7e50fc7782d6d8386b63f29069
NurulNajatMony <nurulmony505@gmail.com>
2025-05-17 21:01:06 +0700

    menambahkan class buku dan friend class

--- latihanB.cpp
+++ latihanB.cpp
@@ -2,3 +2,25 @@
 #include <string>
 using namespace std;
 
+// Class Buku untuk menyimpan informasi buku
+class Buku {
+private:
+    string judul;
+    string penulis;
+    bool dipinjam; // Status peminjaman, privat
+
+public:
+    Buku(string j, string p) : judul(j), penulis(p), dipinjam(false) {}
+
+    // Fungsi untuk melihat status peminjaman
+    bool getDipinjam() const {
+        return dipinjam;
+    }
+
+    // Friend class Petugas agar bisa mengubah status peminjaman
+    friend class Petugas;
+    
+    // Friend class Admin agar bisa melihat data buku
+    friend class Admin;
+};
+
