a6762e28074bbc009717eacce40128dfd1851118
NurulNajatMony <nurulmony505@gmail.com>
2025-05-17 21:04:22 +0700

    menambahkan class peminjaman

--- latihanB.cpp
+++ latihanB.cpp
@@ -24,3 +24,42 @@ public:
     friend class Admin;
 };
 
+// Class Peminjaman untuk menyimpan informasi peminjaman
+class Peminjaman {
+private:
+    string nama;
+    int id;
+    int jumlahBukuDipinjam;
+    int totalPinjaman;
+
+public:
+    Peminjaman(string n, int i) : nama(n), id(i), jumlahBukuDipinjam(0), totalPinjaman(0) {}
+
+    // Friend class Petugas agar bisa mengubah total pinjaman
+    friend class Petugas;
+
+    // Friend class Admin agar bisa mengakses data peminjaman
+    friend class Admin;
+
+    // Fungsi untuk menambah jumlah buku yang dipinjam
+    void tambahBuku(int jumlah) {
+        jumlahBukuDipinjam += jumlah;
+        totalPinjaman += jumlah;
+    }
+
+    // Fungsi untuk mendapatkan jumlah buku yang dipinjam
+    int getJumlahBuku() const {
+        return jumlahBukuDipinjam;
+    }
+
+    // Fungsi untuk melihat total pinjaman
+    int getTotalPinjaman() const {
+        return totalPinjaman;
+    }
+
+    // Fungsi untuk melihat nama peminjam
+    string getNama() const {
+        return nama;
+    }
+};
+
